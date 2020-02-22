fef1bc55e84b5765e11c7b12fc9287a260f68cb5
Dmytro Tus <dmytrotus@gmail.com>
2020-02-22 13:15:46 +0100

    set up this state when form

--- src/components/ReactForm.js
+++ src/components/ReactForm.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function ReactForm(){
+class ReactForm extends Component {
+	constructor(props) {
+		super(props)
 
-	return(
-		
-		<form action="">
-			some form
+		this.state = {
+			name: '',
+			email: ''
+		}
+	}
+
+	changeHandler = (e) => {
+		this.setState({ [e.target.name]: e.target.value })
+	}
+
+	submitHandler = e => {
+		e.preventDefault();
+		console.log(this.state);
+	}
+
+	render(){
+		const { name, email } = this.state
+
+		return(
+			
+		<form onSubmit={this.submitHandler}>
+			<div className="form-group">
+				<input className="form-control" placeholder="Name" type="text"
+					value={name} name="name"
+					onChange={this.changeHandler}
+				/>
+				<input className="form-control" placeholder="email" type="text"
+					value={email} name="email"
+					onChange={this.changeHandler}
+				/>
+				<button className="m-2 btn btn-primary" type="submit">Send Form</button>
+			</div>
 		</form>
-	)
+		)
+	}
 }
 
 export default ReactForm;
