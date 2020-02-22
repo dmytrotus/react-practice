535eebb489eac0a6a2cf03d9bba3e3eb89d7f64c
Dmytro Tus <dmytrotus@gmail.com>
2020-02-22 12:53:14 +0100

    template for react form

---
+++ src/components/ReactForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function ReactForm(){
+
+	return(
+		
+		<form action="">
+			some form
+		</form>
+	)
+}
+
+export default ReactForm;
