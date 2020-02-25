ec473c8f8cc1e58bf1d82754bbbdf33d219f5b7f
Dmytro Tus <dmytrotus@gmail.com>
2020-02-25 08:58:35 +0100

    basic import Class with console log and no directives

---
+++ src/components/MyFirstOopComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../classes/MyNewClass';
+
+
+ function MyFirstOopComponent(){
+
+
+ 	return(
+ 		<div>blabla</div>
+ 	)
+ }
+
+
+ export default MyFirstOopComponent;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ReduxComponent from './components/ReduxComponent';
-import { createStore } from 'redux';
-import allReducer from './reducers';///mistake
-
-import { Provider } from 'react-redux';
-
-import NumberFromRedux from './components/NumberFromRedux';
-
-
-const store = createStore(
-	allReducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	);
+import MyFirstOopComponent from './components/MyFirstOopComponent';
 
 
 
-
-
-ReactDOM.render(
-	<Provider store={store}>
-	<ReduxComponent />
-	</Provider>, document.getElementById("root"));
-
-ReactDOM.render(
-	<Provider store={store}>
-	<NumberFromRedux />
-	</Provider>, document.getElementById("number"));
+ReactDOM.render(<MyFirstOopComponent />, document.getElementById("root"));
