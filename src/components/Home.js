a4521e37c0e262c223bf329cf0376e213546b31a
Dmytro Tus <dmytrotus@gmail.com>
2020-02-28 23:02:04 +0100

    basic router install without package

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home(){
+
+
+	return(
+		<div>
+		Hello, I'm home
+		</div>
+		)
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ReduxComponent from './components/ReduxComponent';
-import { createStore } from 'redux';
-import allReducer from './reducers';///mistake
-
-import { Provider } from 'react-redux';
-
-import NumberFromRedux from './components/NumberFromRedux';
+import Home from './components/Home';
 
 
-const store = createStore(
-	allReducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	);
+function App(){
+
+	return(
+
+			<div>
+			blabla
+
+			<Home />
+			</div>
+		)
+}
 
 
 
 
-
-ReactDOM.render(
-	<Provider store={store}>
-	<ReduxComponent />
-	</Provider>, document.getElementById("root"));
 
 ReactDOM.render(
-	<Provider store={store}>
-	<NumberFromRedux />
-	</Provider>, document.getElementById("number"));
+
+	<App />, document.getElementById("root"));
